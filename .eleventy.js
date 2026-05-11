d8475ed905d3877bcb8c4e9c7c5fa89504b2f027
spencer allan <69729293+sirciv@users.noreply.github.com>
2026-05-11 14:51:28 -0500

    rework deployment logic, color swaps

--- .eleventy.js
+++ .eleventy.js
@@ -2,8 +2,8 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets");
   eleventyConfig.addPassthroughCopy("src/css");
 
-  return {
-    pathPrefix: "/varsity-poet/",
+return {
+    pathPrefix: process.env.ELEVENTY_ENV === "production" ? "/varsity-poet/" : "/",
     dir: {
       input: "src",
       output: "_site",
